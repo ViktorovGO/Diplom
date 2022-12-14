1c87be68947e7379681bf6325d2eaf2529fd38d3
Flash <btovt@rambler.ru>
2022-12-14 19:15:27 +0300

    for pc

--- main.py
+++ main.py
@@ -5,20 +5,44 @@ import pandas as pd
 def out_table(N, M, mx):
     k_l,k_r=map(int,input("Введите пределы изменения длины серий через запятую (левый,правый) - ").split(','))
     shag=int(input("Введите шаг изменения длины серий (целое число) - "))
-    lst=[[],[],[]]
+    m=int(input("Введите по скольким значениям проводить усреднение - "))
+    lst=[[],[],[],[]]
+    
     for k in range(k_l,k_r+1,shag):
+        res_list=[[],[],[],[]]
+        for i in range(m):
+            out_lst=razl(N, M, mx, k,0)
+            res_list[0]=out_lst[0]
+            res_list[1].append(out_lst[1])
+            res_list[2].append(out_lst[2])
+            res_list[3].append(out_lst[3])
 
-        out_lst=razl(N, M, mx, k,0)   
-        lst[0].append(out_lst[0])
-        if (str(type(out_lst[1]))=="<class 'float'>"):
-            lst[1].append(round(out_lst[1],2))
+        if res_list[1].count("-")<=(len(res_list[1])-res_list[1].count("-")):
+            buf=[]
+            for i in range(len(res_list[1])):
+                if str(type(res_list[1][i]))!="<class 'str'>":
+                    buf.append(res_list[1][i])
+            res_list[1]=buf     
+            res_list[1]=mean(res_list[1])
         else:
-            lst[1].append(out_lst[1])     
-        lst[2].append(out_lst[2])  
-        
+            res_list[1]="-"    
 
-    df=pd.DataFrame(lst,columns=list(range(1,(len(lst[0])+1))),index=["K","Тлт","Tзап"])
-    what=input("Вывести таблицу в excel / Вывести таблицу в консоль 'y/n' ")
+        res_list[2]=int(mean(res_list[2]))
+        res_list[3]=int(mean(res_list[3]))
+               
+        lst[0].append(res_list[0])
+        if (str(type(res_list[1]))=="<class 'float'>"):
+            lst[1].append(round(res_list[1],2))
+        else:
+            lst[1].append(res_list[1])     
+        lst[2].append(res_list[2])  
+        lst[3].append(res_list[3])
+        res_list.clear()
+
+    df=pd.DataFrame(lst,columns=list(range(1,(len(lst[0])+1))),index=["Число серий","Тлт","Tзап","Время обнаружения ЛТ"])
+    what=""
+    while what not in ["Y","y","N","n"]:    
+        what=input("Вывести таблицу в excel / Вывести таблицу в консоль 'y/n' ")
     if what=='y':
         df.to_excel("table.xlsx",sheet_name="sheet_1", header=False)
     elif what=='n':
@@ -34,40 +58,42 @@ def razl(N,M,mx,k,out=1):
         k_p=0  # Число положительных серий
         k_n=0  # Число положительных серий
         Tlt=[] # Моменты времени ложных тревог
-        i=0
+        Tr=[] # Моменты леальных тревог
+        j=0
         for i in range(len(x)):    
             
             if x[i]>=med:
                 k_p+=1
-            elif x[i]<=med:
-                k_p=0    
-            if x[i]<=med:
-                k_n+=1
-            elif x[i]>=med:
-                k_n=0 
-
+                k_n=0
+            elif x[i]<med:
+                k_p=0
+                k_p+=1    
+            
+            if i==N:
+                k_n=0
+                k_p=0
             if k_p>=k:
                 if i<N:
                     Tlt.append(i)
                 elif i>=N:
-                    break
+                    Tr.append(i)
                 k_p=0
             elif k_n>=k:
                 if i<N:
                     Tlt.append(i)
                 elif i>=N:
-                    break
+                    Tr.append(i)
                 k_n=0
-        return i
+        return Tr
     
     
-    random.seed(1)
+    
     x=[]
-    x=[random.gauss(0,1) for i in range(N)]
+    x=[(random.random()-0.5)*1 for i in range(N)]
     med=median(x)
-    x.extend([random.gauss(mx,1) for i in range(N,M)])
+    x.extend([(random.random()+mx-0.5)*1 for i in range(N,M)])
 
-    lt=opr_razl(x, med, k, N)
+    Tr=opr_razl(x, med, k, N)
 
     razn=[] # Список разностей между ложными тревогами
 
@@ -85,6 +111,7 @@ def razl(N,M,mx,k,out=1):
     else:
         if len(Tlt)==0:
             mean_razn="-"
+
     if out==1:  
         # Вывод результатов
         print(f'\n\nМедиана - {med}')
@@ -93,22 +120,31 @@ def razl(N,M,mx,k,out=1):
         else:
             print("Ложные тревоги отсутствуют")    
         print(f'Среднее время между ложными тревогами - {mean_razn}')
-        print(f'Время запаздывания - {lt-N}')
-        
+        print(f'Время запаздывания - {Tr[0]-N}')
+
+        x_dot=[]      # Значения ряда в моменты реальных тревог
+        for i in Tlt:
+            x_dot.append(x[i])
+
+        x_dot_real=[] # Значения ряда в моменты реальных тревог
+        for i in Tr:
+            x_dot_real.append(x[i])
+
         plt.plot(range(len(x)),x)
+        plt.plot(Tlt,x_dot,'.',label="Ложная тревога",color="r",markersize=10)
+        plt.plot(Tr,x_dot_real,".",label="Реальная тревога",markersize=10,color="0")
         plt.axhline(med, color='y',label='Медиана до разладки')
         plt.legend()    
         plt.show()
     else:
-        out_lst=[k,mean_razn,lt-N]
+        out_lst=[k,mean_razn,Tr[0]-N,Tr[0]]
         return out_lst
-
-
-
-st=input("Определение разладки / Построить таблицу зависимостей - 'y/n' ")
-N=int(input("Введите момент времени разладки - "))
-M=int(input("Введите общее число моментов времени - "))
-mx=float(input("Введите мат ожидание разладки(исходное=0) - "))
+st=""
+while st not in ["Y","y","N","n"]:
+    st=input("Определение разладки / Построить таблицу зависимостей - 'y/n' ")
+N=int(input("Введите с какого отсчёта пойдёт разладка - "))
+M=int(input("Введите общее число отсчётов - "))
+mx=float(input("Введите медиану процесса начиная с момента заданной разладки(исходная=0) - "))
 
 if st=='y':
     k=int(input("Введите длину серий успехов - "))
